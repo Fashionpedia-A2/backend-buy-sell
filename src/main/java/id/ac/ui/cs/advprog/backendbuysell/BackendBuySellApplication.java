0c14a1c4b3d1e895e05af64134afc6224b7e4dc0
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-04-15 19:57:57 +0700

    spring init

---
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/BackendBuySellApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.backendbuysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBuySellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBuySellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/backendbuysell/BackendBuySellApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.backendbuysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBuySellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
