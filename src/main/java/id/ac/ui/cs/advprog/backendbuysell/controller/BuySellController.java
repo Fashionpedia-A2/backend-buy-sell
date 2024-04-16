e0dc44828b6d1041ca52622a9c36def9d8d715b1
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-04-16 13:42:39 +0700

    preparing for deploymet

---
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/controller/BuySellController.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.backendbuysell.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class BuySellController {
+    @RequestMapping("")
+    public String main(){
+        return new String("Hello world");
+    }
+}
