65ca0956906a149333b35ef25a7f2b77d28fc433
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-04-17 17:14:38 +0700

    PLSS

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/BackendBuySellApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/BackendBuySellApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BackendBuySellApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendBuySellApplication.class, args);
     }
