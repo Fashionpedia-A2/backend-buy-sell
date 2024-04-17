bf06f551830ec9da7cf054763a1b8b22d52eeb48
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-04-17 14:52:10 +0700

    ikut

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
