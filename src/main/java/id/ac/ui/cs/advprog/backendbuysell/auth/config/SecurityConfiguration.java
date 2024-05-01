932bd8a000c8129f90e9d6f9686105323c477ba6
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2024-05-01 22:29:29 +0700

    change request matching to buyer

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/auth/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/auth/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/buyer/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
