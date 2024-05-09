2faa4e6f129c66a1913bed26fa1d2efb77281808
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-09 13:32:53 +0700

    [RED] Add search with filter and paging for Listing Service

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/service/ListingService.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/service/ListingService.java
@@ -1,18 +1,27 @@
 package id.ac.ui.cs.advprog.backendbuysell.service;
 
+import id.ac.ui.cs.advprog.backendbuysell.dto.ListingSearchCriteriaDTO;
 import id.ac.ui.cs.advprog.backendbuysell.model.Listing;
+import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ListingService {
     Listing create(Listing listing);
-    List<Listing> getAll();
-    Listing getById(Long id);
+
+    List<Listing> getAll(ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable);
+
+    Optional<Listing> getById(Long id);
+
     Listing update(Long id, Listing updatedListing);
+
     Listing delete(Long id);
 
-    List<Listing> getAllActiveListing();
-    List<Listing> searchByName();
-    List<Listing> getSellerListing();
+    List<Listing> getActiveListings(ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable);
+
+    List<Listing> getSellerListings(String sellerId, ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable);
+
     void setStatus(Long id, String status);
 }
--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/service/ListingServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/service/ListingServiceImpl.java
@@ -1,37 +1,43 @@
 package id.ac.ui.cs.advprog.backendbuysell.service;
 
+import id.ac.ui.cs.advprog.backendbuysell.dto.ListingSearchCriteriaDTO;
 import id.ac.ui.cs.advprog.backendbuysell.model.Listing;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ListingServiceImpl implements ListingService{
+public class ListingServiceImpl implements ListingService {
     public Listing create(Listing listing) {
         return null;
     }
-    public List<Listing> getAll() {
+
+    public List<Listing> getAll(ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable) {
         return null;
     }
-    public Listing getById(Long id) {
-        return null;
+
+    public Optional<Listing> getById(Long id) {
+        return Optional.empty();
     }
+
     public Listing update(Long id, Listing updatedListing) {
         return null;
     }
+
     public Listing delete(Long id) {
         return null;
     }
 
-    public List<Listing> getAllActiveListing() {
+    public List<Listing> getActiveListings(ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable) {
         return null;
     }
-    public List<Listing> searchByName() {
+
+    public List<Listing> getSellerListings(String sellerId, ListingSearchCriteriaDTO searchCriteriaDTO, Pageable pageable) {
         return null;
     }
-    public List<Listing> getSellerListing() {
-        return null;
-    }
+
     public void setStatus(Long id, String status) {
     }
 }
