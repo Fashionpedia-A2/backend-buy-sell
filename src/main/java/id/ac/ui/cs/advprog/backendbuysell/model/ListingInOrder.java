1bdb744184a83b8d0adcfccd5dc7ffc14de423c0
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-11 14:14:38 +0700

    [GREEN] Implement ListingInOrder model

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/model/ListingInOrder.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/model/ListingInOrder.java
@@ -1,13 +1,19 @@
 package id.ac.ui.cs.advprog.backendbuysell.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.Set;
 
 @Entity(name = "ListingInOrder")
 @Table(name = "listing_in_order")
@@ -20,11 +26,15 @@ public class ListingInOrder {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "listing_id", nullable = false)
+    @NotNull
     private Listing listing;
 
     @Column(name = "quantity", nullable = false)
+    @NotNull
+    @Min(value = 0)
     private Integer quantity;
 
+    @Min(value = 0)
     @Column(name = "sub_total_price", nullable = false)
     private Long subTotalPrice;
 
@@ -35,13 +45,24 @@ public class ListingInOrder {
     }
 
     public ListingInOrder(Listing listing, Integer quantity) {
+        this.listing = listing;
+        this.quantity = quantity;
+        this.calculateSubTotalPrice();
     }
 
-    public void calculateSubTotalPrice(){}
+    public void calculateSubTotalPrice() {
+        assert this.listing != null && this.listing.getPrice() != null && this.quantity != null : "Product or quantity is missing for calculating subtotal price";
+        this.subTotalPrice = Math.multiplyExact(this.listing.getPrice(), this.quantity);
+    }
 
-    public Errors validate(){
-        BindingResult bindingResult = new BeanPropertyBindingResult(this, "listing");
+    public Errors validate() {
+        BindingResult bindingResult = new BeanPropertyBindingResult(this, "listingInOrder");
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<ListingInOrder>> violations = validator.validate(this);
+        for (ConstraintViolation<ListingInOrder> violation : violations) {
+            bindingResult.addError(
+                    new FieldError("listingInOrder", violation.getPropertyPath().toString(), violation.getMessage()));
+        }
         return bindingResult;
     }
 }
