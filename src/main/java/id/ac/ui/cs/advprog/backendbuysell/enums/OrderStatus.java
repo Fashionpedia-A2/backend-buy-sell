3bd2e78a763d0c8788e461d108af2486d0c73cd9
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-11 14:56:49 +0700

    [RED] Add skeleton for Order model

---
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.backendbuysell.enums;
+
+public enum OrderStatus {
+    MENUNGGU_PEMBAYARAN,
+    MENUNGGU_PROSES,
+    DIPROSES,
+    DIKIRIM,
+    SELESAI,
+    DIBATALKAN;
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/model/Order.java
@@ -0,0 +1,83 @@
+package id.ac.ui.cs.advprog.backendbuysell.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import id.ac.ui.cs.advprog.backendbuysell.enums.OrderStatus;
+import jakarta.persistence.*;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Entity(name = "Order")
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Date updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status = OrderStatus.MENUNGGU_PEMBAYARAN;
+
+    @Column(name = "totalPrice", nullable = false)
+    private Long totalPrice;
+
+    @JsonProperty("listings")
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id", nullable = false)
+    private List<ListingInOrder> listingInOrders;
+
+    @Column(name = "seller_id", nullable = false)
+    private String sellerId;
+
+    @Column(name = "buyer_id", nullable = false)
+    private String buyerId;
+
+    @Column(name = "payment_id")
+    private Long paymentId;
+
+
+    public void setStatus(String status) {
+    }
+
+    public void calculateTotalPrice() {
+    }
+
+    public void setListingInOrders(List<ListingInOrder> listingInOrders) {
+    }
+
+    public void addListingInOrder(ListingInOrder listingInOrder) {
+    }
+
+    public void removeListingInOrder(ListingInOrder listingInOrder) {
+    }
+
+    public Errors validate() {
+        BindingResult bindingResult = new BeanPropertyBindingResult(this, "order");
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<Order>> violations = validator.validate(this);
+        for (ConstraintViolation<Order> violation : violations) {
+            bindingResult.addError(
+                    new FieldError("order", violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return bindingResult;
+    }
+}
