0bcd8e38cdc0deab3ed5d8c9e6445b799892f6f1
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-21 03:35:31 +0700

    Make seller non persistent and refers to user profile table

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/model/Seller.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/model/Seller.java
@@ -5,31 +5,42 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Immutable;
 
 import java.util.List;
 
-@Entity
-@Table(name="seller")
+@Setter
 @Getter
 public class Seller {
-    @Setter
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
-    @Setter
-    @Column(name="name")
     private String name;
 
-    @OneToMany(mappedBy = "seller")
     private List<Listing> listing;
-    @Override
-    public String toString() {
-        ObjectMapper om = new ObjectMapper();
-        try {
-            return om.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
+//@Entity
+//@Table(name="seller")
+//@Getter
+//@Immutable
+//public class Seller {
+//    @Setter
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Setter
+//    private String name;
+//
+//    @OneToMany(mappedBy = "sellerId")
+//    private List<Listing> listing;
+//    @Override
+//    public String toString() {
+//        ObjectMapper om = new ObjectMapper();
+//        try {
+//            return om.writeValueAsString(this);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
