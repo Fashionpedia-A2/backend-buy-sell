81de1e0e2b6c415a3ae1ddee6a01ac0eaaf3f34c
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-20 14:00:43 +0700

    Enable all endpoint

--- src/main/java/id/ac/ui/cs/advprog/backendbuysell/auth/config/SecurityConfiguration.java
+++ src/main/java/id/ac/ui/cs/advprog/backendbuysell/auth/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/buyer/**")
+                .requestMatchers("/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
